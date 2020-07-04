dd63e54620b0b44e77e980f6c55797dd96b68884
Mylene Pedone <mylene.pedone@gmail.com>
2020-07-04 19:35:14 -0400

    Initial Commit

---
+++ Swift 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift 2
+//
+//  Created by Mylene Pedone on 7/4/20.
+//  Copyright © 2020 Mylene Pedone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
